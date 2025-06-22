6b6fd1f57f1d3feebe898fa818f1208d91097ecd
Jinwoo Sung <jwsung91@gmail.com>
2025-06-22 18:15:01 +0900

    feat: apply poetry

--- src/serial_gui/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from gui.serial_gui import SerialCommGUI
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    gui = SerialCommGUI()
-    gui.show()
-    sys.exit(app.exec_())
